88c915681fbffd53b4170981ac52e541b3417a80
mo2605446cvg <mo8457ed@gmail.com>
2025-08-01 19:32:04 +0300

    Update MainApplication.kt

--- android/app/src/main/java/com/alsonacademy/MainApplication.kt
+++ android/app/src/main/java/com/alsonacademy/MainApplication.kt
@@ -1,31 +1,27 @@
 package com.alsonacademy
 
 import android.app.Application
-import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.defaults.DefaultReactNativeHostDelegate
+import com.facebook.react.PackageList
 import com.facebook.soloader.SoLoader
 
 class MainApplication : Application(), ReactApplication {
 
-  private val mReactNativeHost: ReactNativeHost = object : DefaultReactNativeHost(this) {
+  private val mReactNativeHost = object : DefaultReactNativeHost(this) {
     override fun getUseDeveloperSupport() = BuildConfig.DEBUG
 
     override fun getPackages(): List<ReactPackage> {
       return PackageList(this).packages
     }
 
-    override fun getJSMainModuleName(): String {
-      return "index"
-    }
+    override fun getJSMainModuleName(): String = "index"
   }
 
-  override fun getReactNativeHost(): ReactNativeHost {
-    return mReactNativeHost
-  }
+  override fun getReactNativeHost(): ReactNativeHost = mReactNativeHost
 
   override fun onCreate() {
     super.onCreate()
