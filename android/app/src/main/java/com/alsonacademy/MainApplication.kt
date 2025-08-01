3911bc317e811fe94930ce18485a5b65c1cc75f9
mo2605446cvg <mo8457ed@gmail.com>
2025-08-01 19:02:36 +0300

    Update MainApplication.kt

--- android/app/src/main/java/com/alsonacademy/MainApplication.kt
+++ android/app/src/main/java/com/alsonacademy/MainApplication.kt
@@ -5,21 +5,30 @@ import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
-import com.facebook.react.defaults.DefaultReactNativeHost
-import com.facebook.react.defaults.DefaultReactNativeHostDelegate
 import com.facebook.soloader.SoLoader
 
 class MainApplication : Application(), ReactApplication {
 
-  private val mReactNativeHost: ReactNativeHost = object : DefaultReactNativeHost(this) {
-    override fun isNewArchEnabled(): Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
-    override fun isHermesEnabled(): Boolean = BuildConfig.IS_HERMES_ENABLED
+  private val mReactNativeHost: ReactNativeHost = object : ReactNativeHost(this) {
+    override fun getUseDeveloperSupport(): Boolean {
+      return BuildConfig.DEBUG
+    }
+
+    override fun getPackages(): List<ReactPackage> {
+      return PackageList(this).packages
+    }
+
+    override fun getJSMainModuleName(): String {
+      return "index"
+    }
   }
 
-  override fun getReactNativeHost(): ReactNativeHost = mReactNativeHost
+  override fun getReactNativeHost(): ReactNativeHost {
+    return mReactNativeHost
+  }
 
   override fun onCreate() {
     super.onCreate()
-    SoLoader.init(this, false)
+    SoLoader.init(this, /* native exopackage */ false)
   }
 }
